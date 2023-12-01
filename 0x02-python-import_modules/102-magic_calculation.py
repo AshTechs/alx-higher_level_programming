2435a22b419f98435c6f482e70052954338c0def
AshTechs <ashbelh@gmail.com>
2023-11-30 16:31:04 -0800

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+def magic_calculation(a, b):
+    if x < y:
+        z = add(x, y)
+        for a in range(4, 6):
+            z = add(z, a)
+            return (z)
+        else:
+            return sub(x, y)
