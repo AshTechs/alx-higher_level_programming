bcdd4100391890f915dbc62bdcaa64a32510cc93
AshTechs <ashbelh@gmail.com>
2023-11-30 17:30:13 -0800

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from variable_load_5 import a
+from variable_load_5 import i
 def find_var():
-    print(a)
-if __name__ == "__main__":
-    find_var()
+    print(i)
+    if __name__ == "__main__":
+        find_var()
