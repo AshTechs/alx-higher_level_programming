d0211368733b17b051d6497ceb2fe2285746f681
AshTechs <ashbelh@gmail.com>
2023-12-04 20:42:56 -0800

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    
+    sum_1 = tuple_a[0] if len_a >= 1 else 0
+    sum_2 = tuple_a[1] if len_a >= 2 else 0
+    sum_3 = tuple_b[0] if len_b >= 1 else 0
+    sum_4 = tuple_b[1] if len_b >= 2 else 0
+    
+    new_tuple = (sum_1 + sum_3, sum_2 + sum_4)
+    
+    return new_tuple
