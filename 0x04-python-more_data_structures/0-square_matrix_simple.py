4fc7191385633e77ed96e2081df1584b5bdc0616
AshTechs <ashbelh@gmail.com>
2023-12-08 16:20:36 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix):
+    rows, cols = len(matrix), len(matrix[0])
+    result = [[0] * cols for _ in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            result[i][j] = matrix[i][j] ** 2
+    return result
