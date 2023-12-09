b3aa9a064ed7df5d9ff075304bcbcbc511f75c48
AshTechs <ashbelh@gmail.com>
2023-12-08 18:07:07 -0800

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    return {key: val for key, val in a_dictionary.items() if val != value}
-
+def complex_delete(my_dict, value):
+    tmp = my_dict.copy()
+    for i, j in tmp.items():
+        if value == j:
+            my_dict.pop(i)
+    return my_dict
