44451dd2bc0cfa4bca8b6a9258cf6445450115c8
AshTechs <ashbelh@gmail.com>
2023-12-18 15:50:28 -0800

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{}".format(value).strip())
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
