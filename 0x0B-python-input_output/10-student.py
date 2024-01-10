e69a92a471945b05bb3c5b65d54401646c991c29
AshTechs <ashbelh@gmail.com>
2024-01-09 17:40:05 -0800

    Correct 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -28,5 +28,5 @@ class Student:
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
-            return {i: getattr(self, k) jor i in attrs if hasattr(self, i)}
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
         return self.__dict__
