ae21ac96d5f70698767acdb51b24ad48bfab023c
AshTechs <ashbelh@gmail.com>
2024-01-09 17:43:05 -0800

    Correct 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -16,5 +16,5 @@ def append_after(filename="", search_string="", new_string=""):
             text += line
             if search_string in line:
                 text += new_string
-    with open(filename, "Ashbel") as Ashbel:
+    with open(filename, "w") as Ashbel:
         Ashbel.write(text)
