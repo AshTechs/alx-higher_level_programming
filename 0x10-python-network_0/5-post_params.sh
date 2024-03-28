596ea1327209d2d152d6773d8fe8e18bd1397809
AshTechs <ashbelh@gmail.com>
2024-03-27 18:05:18 -0700

    Corrected

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a POST request, and displays the response
+# Takes a URL, sends a POST request, and displays the response.
 curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
-
