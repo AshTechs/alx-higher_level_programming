3b35b9003bdf4840f01eb78ac27ead634cf51d7f
AshTechs <ashbelh@gmail.com>
2024-03-27 18:40:54 -0700

    Corrected

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes a URL, sends a GET request, and displays the response.
-curl -s -o response.txt -w "%{http_code}" $1 | awk 'NR==1{if($1 == 200) {getline; print}}'
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1"
