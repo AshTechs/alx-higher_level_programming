f710eb887991305b17bb2a9e0f4385ec0b10dd92
AshTechs <ashbelh@gmail.com>
2024-04-01 13:31:04 -0700

    Corrected

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,23 @@ This module fetches data from a given URL using requests package.
 
 import requests
 
-url = 'https://alx-intranet.hbtn.io/status'
+def fetch_status_with_requests(url):
+    """
+    Fetches the status from the given URL using requests package.
 
-response = requests.get(url)
-for line in response.text.split("\n"):
-    print('- {}'.format(line))
+    Args:
+        url (str): The URL to fetch the status from.
+
+    Returns:
+        None
+    """
+    response = requests.get(url)
+    content_type = response.headers.get('Content-Type', 'Unknown')
+    content = response.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+
+if __name__ == "__main__":
+    fetch_status_with_requests("https://alx-intranet.hbtn.io/status")
