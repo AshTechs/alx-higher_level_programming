d81135cd9d13f406c411cc9ad6b3b93a12b1ea70
AshTechs <ashbelh@gmail.com>
2024-04-01 16:17:25 -0700

    Corrected

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
 
 """
-This script lists 10 commits (from most recent to oldest) of a given repositor
+Retrieves information about a given repository using requests package
 """
 
-import sys
 import requests
+import sys
 
-def list_commits(repo_name, owner_name):
-
+def get_repository_info(repo_name, owner_name):
     """
-    Lists 10 commits (from the most recent to oldest) of a given repository
+    Retrieves information about a given repository using GitHub API
 
     Args:
         repo_name (str): The name of the repository.
-        owner_name (str): The owner of the repository.
+        owner_name (str): The name of the repository owner.
 
     Returns:
         None
     """
-    
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-    params = {"per_page": 10}
-    response = requests.get(url, params=params)
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}"
+    response = requests.get(url)
     if response.ok:
-        commits = response.json()
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
+        data = response.json()
+        print(data)
     else:
         print("Error:", response.text)
 
