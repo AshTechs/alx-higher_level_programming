7eb8c08648beaf65e176c92b125688e858d449d1
AshTechs <ashbelh@gmail.com>
2024-04-01 13:16:14 -0700

    Corrected

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -21,13 +21,17 @@ def fetch_status(url):
         with urllib.request.urlopen(url) as response:
             content = response.read()
             utf8_content = content.decode('utf-8')
+            response_info = {'type': type(content), 'content': content, 'utf8_content': utf8_content}
     except urllib.error.URLError as e:
-        print(e.reason)
-    else:
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(utf8_content))
+        response_info = {'error_message': e.reason}
+    
+    print("Body response:")
+    print("\t- type: {}".format(response_info['type'] if 'type' in response_info else 'Unknown'))
+    print("\t- content: {}".format(response_info['content'] if 'content' in response_info else 'Unknown'))
+    print("\t- utf8 content: {}".format(response_info['utf8_content'] if 'utf8_content' in response_info else 'Unknown'))
+
+    if 'error_message' in response_info:
+        print("Error:", response_info['error_message'])
 
 if __name__ == "__main__":
     fetch_status("https://alx-intranet.hbtn.io/status")
