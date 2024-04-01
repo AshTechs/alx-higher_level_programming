7bddcb97c6badfddae62f88e9e254898cdc3e07d
AshTechs <ashbelh@gmail.com>
2024-04-01 14:12:29 -0700

    Corrected

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 This script fetches the user ID using GitHub API with Basic Authentication.
 """
@@ -9,23 +10,23 @@ import requests
 def get_github_id(username, password):
     """
     Retrieves the user ID using GitHub API with Basic Authentication.
+
     Args:
         username (str): The GitHub username.
         password (str): The personal access token.
+
     Returns:
         None
     """
-
     url = "https://api.github.com/user"
-    headers = {"Authorization": f"token {password}"}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
+    auth = (username, password)
+    response = requests.get(url, auth=auth)
+    if response.ok:
         data = response.json()
         print(data['id'])
     else:
         print("Error:", response.text)
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
+    username, password = sys.argv[1:3]
     get_github_id(username, password)
