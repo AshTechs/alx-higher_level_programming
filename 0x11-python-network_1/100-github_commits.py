7e283b9e12e2d21ea5a9fea954a5335bafcfe785
AshTechs <ashbelh@gmail.com>
2024-04-01 15:29:04 -0700

    Corrected

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
 
 """
-Retrieves information about a given repository using requests package
+This script lists 10 commits (from most recent to oldest) of a given repositor
 """
 
-import requests
 import sys
+import requests
 
-
-def get_repository_info(repo_name, owner_name):
+def list_commits(repo_name, owner_name):
     """
-    Retrieves information about a given repository.
+    Lists 10 commits (from the most recent to oldest) of a given repository
 
     Args:
         repo_name (str): The name of the repository.
-        owner_name (str): The name of the repository owner.
+        owner_name (str): The owner of the repository.
 
     Returns:
         None
     """
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}"
-    response = requests.get(url)
-    data = response.json()
-    print(data)
-
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    params = {"per_page": 10}
+    response = requests.get(url, params=params)
+    if response.ok:
+        commits = response.json()
+        for commit in commits:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error:", response.text)
 
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    get_repository_info(repo_name, owner_name)
+    list_commits(repo_name, owner_name)
