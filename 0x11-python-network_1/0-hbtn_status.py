0a5d53000b59deba291caee12aac49363a0ed61d
AshTechs <ashbelh@gmail.com>
2024-04-01 17:28:59 -0700

    Good

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ This module fetches data from a given URL using urllib package.
 import urllib.request
 import urllib.error
 
+
 def fetch_status(url):
     """
     Fetches the status from the given URL using urllib.
@@ -21,18 +22,30 @@ def fetch_status(url):
         with urllib.request.urlopen(url) as response:
             content = response.read()
             utf8_content = content.decode('utf-8')
-            response_info = {'type': type(content), 'content': content, 'utf8_content': utf8_content}
+            response_info = {
+                    'type': type(content),
+                    'content': content,
+                    'utf8_content': utf8_content
+            }
     except urllib.error.URLError as e:
         response_info = {'error_message': e.reason}
-    
+
     print("Body response:")
-    print("\t- type: {}".format(response_info['type'] if 'type' in response_info else 'Unknown'))
-    print("\t- content: {}".format(response_info['content'] if 'content' in response_info else 'Unknown'))
-    print("\t- utf8 content: {}".format(response_info['utf8_content'] if 'utf8_content' in response_info else 'Unknown'))
+    print("\t- type: {}".format(
+        response_info['type'] if 'type' in response_info else 'Unknown'
+        ))
+    print("\t- content: {}".format(
+        response_info['content'] if 'content' in response_info else 'Unknown'
+        ))
+    print("\t- utf8 content: {}".format(
+        response_info['utf8_content']
+        if 'utf8_content' in response_info
+        else 'Unknown'
+        ))
 
     if 'error_message' in response_info:
         print("Error:", response_info['error_message'])
 
+
 if __name__ == "__main__":
     fetch_status("https://alx-intranet.hbtn.io/status")
-
