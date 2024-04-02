1dd9f6f73a4d0986a42ecb7c2520322358b31152
AshTechs <ashbelh@gmail.com>
2024-04-01 18:24:05 -0700

    Good

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to a specified URL and displays the body of the response if the status code is 200
 curl -s -o /dev/null -w "%{http_code}" $1 | [ "$(cat)" == "200" ] && curl -s $1
-
