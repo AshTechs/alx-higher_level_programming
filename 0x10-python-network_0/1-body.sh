4dfa41ae07383995afc4d9a5db9342dea7857f97
AshTechs <ashbelh@gmail.com>
2024-04-01 18:23:28 -0700

    Good

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# This script takes a URL as input, sends a GET request to it, and displays the body of the response for a 200 status code with one redirection and for other status codes with five redirections
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1" || curl -s -o /dev/null -w "%{http_code}" "$1" 2>&1 >/dev/null | grep -qE '30[0-9]|404|50[0-9]'
+# This script sends a GET request to a specified URL and displays the body of the response if the status code is 200
+curl -s -o /dev/null -w "%{http_code}" $1 | [ "$(cat)" == "200" ] && curl -s $1
+
