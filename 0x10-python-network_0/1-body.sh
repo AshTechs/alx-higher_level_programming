2134c5e7f684df1aa7625ea33682ef46c4373670
AshTechs <ashbelh@gmail.com>
2024-04-01 18:14:54 -0700

    Good

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes a URL, sends a GET request, and displays the response.
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" = "200" ] && curl -s "$1"
+# This script takes a URL as input, sends a GET request to it, and displays the body of the response for a 200 status code
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
